12b16b4e10de6b58d4a51aeb24fbc3554a106db5
Toni Näsman <toni.nasman@student.hamk.fi>
2025-02-14 13:06:22 +0200

    Popup regin/login/logout

---
+++ tiimi12/popup/check_login.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+if (isset($_SESSION["kayttaja"])) {
+    echo json_encode(["status" => "loggedin", "user" => $_SESSION["kayttaja"]]);
+}
+else {
+    echo json_encode(["status" => "notloggedin"]);
+}
+?>
